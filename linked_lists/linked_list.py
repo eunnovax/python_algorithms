ef8471b4fceb61b12287e62b17309d4f72176ea4
Max Plank <darkhan.r4industry@gmail.com>
2020-06-28 15:19:00 +0600

    reversed linked list

--- linked_lists/linked_list.py
+++ linked_lists/linked_list.py
@@ -1,3 +1,10 @@
+import logging
+from copy import copy, deepcopy
+
+logging.basicConfig(filename='myLinkedListLog.txt', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+logging.debug('Start of the program')
+
 class Node(object):
     def __init__  (self, d, n = None):
         self.data = d
@@ -64,20 +71,49 @@ class LinkedList(object):
                 this_node = this_node.get_next()
         return False
 
+    def reverse(self):
+        logging.debug('Head of LL %s' % (self.root.data))
+        this_node = self.root
+        prev_node = None
+        while this_node:
+            temp_node = deepcopy(this_node)
+            if this_node.get_next() is None:
+                logging.debug('This node next data is None')
+            else:
+                logging.debug('This node next data is %s' % (this_node.get_next().data))
+            if prev_node:
+                temp_node.set_next(prev_node)
+                logging.debug('Temp next node is %s' % (temp_node.get_next().data))
+            else:
+                temp_node.set_next(None)
+                logging.debug('Temp next node is %s' % (temp_node.get_next()))
+            prev_node = temp_node
+            logging.debug('Prev node is %s' % (prev_node.get_data()))
+            logging.debug('This node next is %s' % (this_node.get_next()))
+            this_node = this_node.get_next()
+        self.root = prev_node
+        logging.debug('Head node is %s' % (self.root.data))
+        return True
+
 ll = LinkedList()
 ll.add(4)
 ll.add(3)
 ll.add(40)
 ll.add(23)
 # print(ll.size)
-ll.remove(23)
-print(ll.remove(40))
-print(ll.size)
-print(ll.find(40))
-print(ll.update(50, 3))
+# ll.remove(23)
+# print(ll.remove(40))
+# print(ll.size)
+# print(ll.find(40))
+# print(ll.update(50, 3))
+# print(ll.root.data)
+# print(ll.root.next_node.data)
+# print(ll.root.next_node.next_node.data)
+# print(ll.root.next_node.next_node.next_node.data)
+# print(ll.root.next_node.next_node.next_node.next_node)
+print(ll.reverse())
 print(ll.root.data)
 print(ll.root.next_node.data)
-# print(ll.root.next_node.next_node.data)
-# print(ll.root.next_node.next_node.next_node)
-# print(ll.root.next_node.next_node.next_node.next_node)
+print(ll.root.next_node.next_node.data)
+print(ll.root.next_node.next_node.next_node.data)
 
