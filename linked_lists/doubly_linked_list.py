798dbf393a12e6b1affc54a305983666bfa0fca6
Max Plank <darkhan.r4industry@gmail.com>
2020-06-30 19:51:02 +0600

    doubly linked list print forward and print reverse

--- linked_lists/doubly_linked_list.py
+++ linked_lists/doubly_linked_list.py
@@ -31,10 +31,32 @@ class DoublyLinkedList(object):
             self.root.prev_node = new_node
             self.root = new_node
         self.size += 1
+    def print(self):
+        cur_node = self.root
+        print('Forward: ')
+        while(cur_node != None):
+            print(cur_node.data)
+            cur_node = cur_node.get_next()
+    def reversePrint(self):
+        cur_node = self.root
+        if(cur_node == None):
+            return None
+        # going to the last node
+        while(cur_node.get_next() != None):
+            cur_node = cur_node.get_next()
+        # traversing backwards using prev pointer
+        print('Reverse: ')
+        while(cur_node != None):
+            print(cur_node.data)
+            cur_node = cur_node.get_prev()
+
 
 dll = DoublyLinkedList()
 dll.add(34)
 dll.add(9)
-print(dll.root.data)
-print(dll.root.next_node.data)
-print(dll.root.next_node.prev_node.data)
+dll.add(3)
+# print(dll.root.data)
+# print(dll.root.next_node.data)
+# print(dll.root.next_node.prev_node.data)
+print(dll.print())
+print(dll.reversePrint())
