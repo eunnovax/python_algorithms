9cd48b71404dca193c29bd3aef415eac5461d412
Max Plank <darkhan.r4industry@gmail.com>
2020-07-05 20:46:28 +0600

    adjancency matrix and list implementation of graph

---
+++ graphs/adjacencyList.py
@@ -0,0 +1,49 @@
+# Adjacency List implementation of graph
+
+class Vertex():
+    def __init__(self, n):
+        self.name = n
+        self.neighbors = list()
+
+    def add_neighbor(self, v):
+        if v not in self.neighbors:
+            self.neighbors.append(v)
+            self.neighbors.sort()
+
+class Graph():
+    vertices = {}
+
+    def add_vertex(self, vertex):
+        if isinstance(vertex, Vertex) and vertex.name not in self.vertices:
+            self.vertices[vertex.name] = vertex
+            return True
+        else:
+            return False
+
+    def add_edge(self, u, v):
+        if u in self.vertices and v in self.vertices:
+            for key, value in self.vertices.items():
+                if key == u:
+                    value.add_neighbor(v)
+                if key == v:
+                    value.add_neighbor(u)
+            return True
+        else:
+            return False
+
+    def print_graph(self):
+        for key in sorted(list(self.vertices.keys())):
+            print(key + str(self.vertices[key].neighbors))
+
+g = Graph()
+c = Vertex('C')
+g.add_vertex(c)
+g.add_vertex(Vertex('A'))
+for i in range(ord('A'), ord('L')):
+    g.add_vertex(Vertex(chr(i)))
+
+edges = ['AB', 'AE', 'BF', 'CG', 'DE', 'DH', 'EH', 'FG', 'FJ', 'GJ', 'HI']
+for edge in edges:
+    g.add_edge(edge[:1], edge[1:])
+
+g.print_graph()
