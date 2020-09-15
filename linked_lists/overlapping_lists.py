ecc2ab126161b2f6b133b124a3f748618b067c6b
Max Plank <darkhan.r4industry@gmail.com>
2020-09-15 17:49:23 +0600

    overlapping lists test

---
+++ linked_lists/overlapping_lists.py
@@ -0,0 +1,39 @@
+class ListNode():
+    def __init__(self, x = 0, next = None):
+        self.data = x
+        self.next = next
+
+def overlapping_no_cycle_lists(L1, L2):
+    n1, n2, step1, step2 = L1, L2, 0, 0
+    while n1:
+        n1 = n1.next
+        step1 += 1
+
+    while n2:
+        n2 = n2.next
+        step2 += 1
+
+    if step2 > step1:
+        L2, L1 = L1, L2 # L1 is the longer list
+    diff = abs(step1 - step2)
+    for _ in range(diff):
+        L1 = L1.next
+    while L1 and L2 and L2 is not L1:
+        L2 = L2.next
+        L1 = L1.next
+    return L1.data
+
+l1_head = ListNode(0)
+l1_tail = l1_head
+for i in range(1, 5):
+    l1_tail.next = ListNode(i)
+    l1_tail = l1_tail.next
+
+l2_head = ListNode(0)
+l2_tail = l2_head
+for i in range(1, 5):
+    l2_tail.next = ListNode(i*2)
+    l2_tail = l2_tail.next
+l2_tail.next = l1_head.next.next
+
+print(overlapping_no_cycle_lists(l1_head, l2_head))
