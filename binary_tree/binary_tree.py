00bbf2f17a58571df06398e69cdeb8498c3c90e1
Max Plank <darkhan.r4industry@gmail.com>
2020-10-07 16:35:55 +0600

    lca algorithm updated

--- binary_tree/binary_tree.py
+++ binary_tree/binary_tree.py
@@ -1,3 +1,5 @@
+import collections
+
 class BTNode():
     def __init__(self, data=0):
         self.left = None
@@ -33,5 +35,54 @@ class BinaryTree(): # fills nodes left-to-right using queue
             else:
                 q.append(temp.right)
 
-   
-    
+# class Ancestor():
+#     def __init__(self, node, status):
+#         self.node = node
+#         self.status = status
+
+def lca(tree, node1, node2):
+    Ancestor = collections.namedtuple('Ancestor', ('node','status'))
+    def lca_finder(tree, node1, node2):
+        # base case
+        if not tree:
+            return Ancestor(None, 0)
+        # visit left
+        left = lca_finder(tree.left, node1, node2)
+        # print('left', left)
+        if left.status == 2:
+            return left
+        # visit right
+        right = lca_finder(tree.right, node1, node2)
+        # print('right', right)
+        if right.status == 2:
+            return right
+        # print data
+        # print('left status', left.status, 'right status', right.status)
+        status = right.status + left.status
+        
+        if (tree is node1) or (tree is node2):
+            status += 1
+            print('status inrcemented', status)
+        # print('node data', tree.data)
+        if status == 2:
+            return Ancestor(tree, status)
+        else:
+            return Ancestor(None, status)
+        
+    return lca_finder(tree, node1, node2).node.data
+
+
+tree = BinaryTree(3)
+tree.insert(21)
+tree.insert(12)
+tree.insert(5)
+tree.insert(8)
+tree.insert(0)
+tree.insert(13)
+tree.insert(73)
+tree.insert(69)
+
+node1 = tree.root.right.right
+node2 = tree.root.right.left
+
+print(lca(tree.root, node1, node2))
--- binary_tree/lca.py
+++ binary_tree/lca.py
@@ -1,16 +1,22 @@
 def lca(tree, node1, node2):
+    Ancestor = collections.namedtuple('Ancestor', ('node','status'))
     def lca_finder(tree, node1, node2):
         # base case
-        if tree is node1 or tree is node2:
-            return tree
+        if not tree or tree is node1 or tree is node2:
+            return Ancestor(None, 'not found')
         # visit left
         left = lca_finder(tree.left, node1, node2)
+        if left.status == 'found':
+            return left
         # visit right
         right = lca_finder(tree.right, node1, node2)
+        if right.status == 'found':
+            return right
         # print data
-        if left is not right:
-            return tree
-        elif left is right:
-            return left
-    return lca_finder(tree, node1, node2)
+        if left.node is right.node:
+            return Ancestor(left.node, 'found')
+        else:
+            return Ancestor(tree, 'not found')
+        
+    return lca_finder(tree, node1, node2).node
 
